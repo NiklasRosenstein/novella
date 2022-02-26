4a475cd3dacd028aac8b45d65d738ee0f7baf5aa
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-26 22:01:07 +0100

    hygiene: cleanup build logic

--- docs/content/index.md
+++ docs/content/index.md
@@ -1,4 +1,4 @@
 @anchor docs:novella:home
 # Welcome to the Novella documentation!
 
-@cat ../../readme.md :with slice_lines = "2:"
+@cat ../../readme.md :with slice_lines = "2:-3"
--- src/novella/build.py
+++ src/novella/build.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import abc
-import contextlib
 import enum
 import logging
 import threading
@@ -16,6 +15,7 @@ import watchdog.observers  # type: ignore[import]
 from novella.action import ActionAborted  # type: ignore[import]
 
 if t.TYPE_CHECKING:
+  import contextlib
   from novella.action import Action
   from novella.novella import NovellaContext
 
@@ -54,150 +54,174 @@ class BuildContext(abc.ABC):
     execution is paused (only relevant if your action operates mostly in-memory). """
 
 
+class _FsEventHandler(watchdog.events.FileSystemEventHandler):
+  """ Re-executes the pipeline on a filesystem event. """
+
+  def __init__(self, builder: NovellaBuilder, min_interval: float = 1) -> None:
+    super().__init__()
+    self.builder = builder
+    self.min_interval = min_interval
+    self._last_reload: float | None = None
+
+  def on_any_event(self, event: watchdog.events.FileSystemEvent) -> None:
+    # Enforce the minimum interval between updates.
+    ctime = time.time()
+    if self._last_reload and (ctime - self._last_reload) < self.min_interval:
+      return
+    self._last_reload = ctime
+
+    with self.builder._cond:
+      current = self.builder._current_action
+      if current and current.supports_reloading:
+        # Execute the pipeline in a new builder up until the given action.
+        sub_builder = NovellaBuilder(
+          context=self.builder._context,
+          build_directory=self.builder.directory,
+          stop_before_action=current.name,
+          is_inner=True,
+        )
+        sub_builder.build()
+        return
+      else:
+        self.builder._abort()
+
+
 class NovellaBuilder(BuildContext):
   """ Handles the execution of the Novella pipeline. """
 
-  class Status(enum.Enum):
-    PENDING = enum.auto()
-    STARTING = enum.auto()
-    RUNNING = enum.auto()
-    STOPPED = enum.auto()
-
-  class EventHandler(watchdog.events.FileSystemEventHandler):
-
-    def __init__(self, builder: NovellaBuilder) -> None:
-      self._builder = builder
-
-    def on_any_event(self, event: watchdog.events.FileSystemEvent) -> None:
-      builder = self._builder
-      with builder._lock:
-        # Prevent multiple reloads in a small timeframe.
-        if builder._last_reload is not None and time.time() - builder._last_reload < 1:
-          return
-        if builder._status != NovellaBuilder.Status.RUNNING:
-          return
-        builder._last_reload = time.time()
-        builder._status = NovellaBuilder.Status.STARTING
-        current_action = builder._current_action
-
-      logger.info('<fg=blue;attr=bold>Detected file changes, re-executing pipeline ...</fg>')
-      if current_action and current_action.supports_reloading:
-        logger.info('  Keeping <fg=blue>%s</fg> alive.', current_action.get_description())
-        builder._run_actions(builder._past_actions, True)
-        with builder._lock:
-          builder._status = NovellaBuilder.Status.RUNNING
-      elif current_action:
-        with builder._lock:
-          if builder._abort_callback:
-            builder._abort_callback()
-            builder._abort_callback = None
-
-  def __init__(self, context: NovellaContext, build_directory: Path | None, enable_reloading: bool = False) -> None:
-    import contextlib
-
+  def __init__(
+    self,
+    context: NovellaContext,
+    build_directory: Path | None,
+    enable_reloading: bool = False,
+    stop_before_action: str | None = None,
+    is_inner: bool = False,
+  ) -> None:
     self._context = context
-    self._actions = context.get_actions_ordered()
-    self._past_actions: list[Action] = []
-    self._build_directory = build_directory
-    self._current_action: Action | None = None
-    self._exit_stack = contextlib.ExitStack()
-    self._observer = watchdog.observers.Observer()
-    self._lock = threading.RLock()
-    self._last_reload: float | None = None
-    self._status = NovellaBuilder.Status.PENDING
     self._enable_reloading = enable_reloading
+    self._build_directory = build_directory
+    self._stop_before_action = stop_before_action
+    self._is_inner = is_inner
+
+    self._actions = context.get_actions_ordered()
+    self._current_action: Action | None = None
+    self._current_action_abort: t.Callable[[], t.Any] | None = None
+    self._aborted = False
+    self._finished = False
+    self._cond = threading.Condition(threading.RLock())
+    self._observer = watchdog.observers.Observer()
+    self._event_handler = _FsEventHandler(self)
+
+  def _is_finished(self) -> bool:
+    with self._cond:
+      return self._finished
 
   def _create_temporary_directory(self, exit_stack: contextlib.ExitStack) -> None:
     import tempfile
     assert not self._build_directory
-    assert self._status == NovellaBuilder.Status.STARTING
     tmpdir = exit_stack.enter_context(tempfile.TemporaryDirectory(prefix='novella-'))
     logger.info('Created temporary build directory <fg=yellow>%s</fg>', tmpdir)
     self._build_directory = Path(tmpdir)
     exit_stack.callback(setattr, self, '_build_directory', None)
 
-  def _run_actions(self, actions: t.Sequence[Action], off_record: bool = False) -> Status:
+  def _run_actions(self) -> None:
     from novella.novella import PipelineError
 
-    past_actions = [] if off_record else self._past_actions
-    for action in actions:
-      if not off_record:
-        with self._lock:
-          self._current_action = action
-          logger.debug('Executing action <info>%s</info>', action.get_description())
+    for action in self._actions:
+
+      if self._stop_before_action and action.name == self._stop_before_action:
+        break
+
+      logger.debug('Executing action <info>%s (%s)</info>', action.name, action.get_description() or '')
+      with self._cond:
+        self._current_action = action
+        self._cond.notify_all()
+
       self.notify(action, 'before_execute', None)
+
       try:
         action.execute(self)
-      except ActionAborted as exc:
-        if self.is_aborted():
-          pass
-        else:
+
+      # It's ok to raise this exception if the action was aborted if #is_aborted() returns True.
+      except ActionAborted:
+        if not self.is_aborted():
           raise
+
+      # Any other exception is converted into a PipelineError.
       except Exception as exc:
+        with self._cond:
+          self._finished = True
+          self._cond.notify_all()
         raise PipelineError(action.name, action.callsite) from exc
+
       finally:
-        if not off_record:
-          with self._lock:
-            self._current_action = None
-      past_actions.append(action)
 
-      if not off_record:
-        with self._lock:
-          if self._status == NovellaBuilder.Status.STARTING:
-            break  # If the status has transitioned back to STARTING, it means we want to restart.
+        with self._cond:
+          self._current_action = None
+          self._current_action_abort = None
+          self._cond.notify_all()
 
-    with self._lock:
-      return self._status
-
-  def watch(self, path: Path) -> None:
-    """ Watch the path for changes to file contents. If any file contents change, the Novella pipeline is
-    executed again (but the build script will not be reloaded). After a restart, the observer is reset. """
-
-    # TODO: What's not so good right now is that on a reload, actions might call this again
-    #   and append the same path again to the observer.
-    self._observer.schedule(NovellaBuilder.EventHandler(self), path, recursive=True)
-
-  def notify(self, action: Action, event: str, commit: t.Callable[[], t.Any] | None = None) -> None:
-    pass
-
-  def is_aborted(self) -> bool:
-    # Build is starting again, so current actions should abort.
-    with self._lock:
-      return self._status == NovellaBuilder.Status.STARTING
-
-  def on_abort(self, callback: t.Callable[[], t.Any]) -> None:
-    with self._lock:
-      if self.is_aborted():
-        callback()
-      else:
-        self._abort_callback = callback
+    # All actions passed, only if we didn't abort them.
+    with self._cond:
+      if not self._aborted:
+        self._finished = True
+        self._cond.notify_all()
 
   def build(self) -> None:
-    with self._lock:
-      if self._status != NovellaBuilder.Status.PENDING:
-        raise RuntimeError('cannot restart build with same NovellaBuilder instance')
-      self._status = NovellaBuilder.Status.STARTING
-      assert self._past_actions == []
+    import contextlib
 
-    with self._exit_stack:
+    with contextlib.ExitStack() as exit_stack:
 
       # Check if any action supports reloading and enable file watching.
-      if self._enable_reloading or any(action.supports_reloading for action in self._actions):
-        self._observer = watchdog.observers.Observer()
+      if not self._is_inner and (self._enable_reloading or any(action.supports_reloading for action in self._actions)):
+        logger.debug('Watching for changes in file system ...')
         self._observer.start()
-        self._exit_stack.callback(self._observer.join)
-        self._exit_stack.callback(self._observer.stop)
+        exit_stack.callback(self._observer.join)
+        exit_stack.callback(self._observer.stop)
 
-      while True:
-        with contextlib.ExitStack() as local_exit_stack:
-          if not self._build_directory:
-            self._create_temporary_directory(local_exit_stack)
-          with self._lock:
-            self._status = NovellaBuilder.Status.RUNNING
-          if self._run_actions(self._actions, False) != NovellaBuilder.Status.STARTING:
-            break  # All actions have run without interruption
+      if not self._build_directory:
+        self._create_temporary_directory(exit_stack)
+
+      while not self._is_finished():
+        with self._cond:
+          self._aborted = False
+          self._cond.notify_all()
+        self._run_actions()
+
+  def _abort(self) -> None:
+    """ Abort the current run of actions. """
+
+    with self._cond:
+      self._aborted = True
+      self._cond.notify_all()
+      callback, self._current_action_abort = self._current_action_abort, None
+
+    if callback:
+      callback()
+
+  # BuildContext
 
   @property
   def directory(self) -> Path:  # type: ignore
     assert self._build_directory
     return self._build_directory
+
+  def watch(self, path: Path) -> None:
+    """ Watch the path for changes to file contents. If any file contents change, the Novella pipeline is
+    executed again (but the build script will not be reloaded). After a restart, the observer is reset. """
+
+    self._observer.schedule(self._event_handler, path, recursive=True)
+
+  def is_aborted(self) -> bool:
+    with self._cond:
+      return self._aborted
+
+  def on_abort(self, callback: t.Callable[[], t.Any]) -> None:
+    with self._cond:
+      if self.is_aborted():
+        callback()
+      else:
+        self._current_action_abort = callback
+
+  def notify(self, action: Action, event: str, commit: t.Callable[[], t.Any] | None = None) -> None:
+    pass
