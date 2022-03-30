71d2d600935041bec1126be5b0a1c660b87c2027
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-03-30 05:19:21 +0200

    improvement: The `@cat` tag now copies image files referenced in the source file into the `MarkdownPreprocessor.path / "img"` directory if needed and adjust the image reference to ensure that it points to the right file

--- src/novella/markdown/tags/cat.py
+++ src/novella/markdown/tags/cat.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import logging
+import shutil
 import typing as t
 from pathlib import Path
 
@@ -19,6 +20,10 @@ class CatTagProcessor(MarkdownPreprocessor):
   starts with a slash, the path is considered relative to the project root directory (the one where the Novella
   build file resides in). The filename is resolved in the project directory (not the temporary build directory).
 
+  If image references are found in the source file, they are updated to point to a path relative to the
+  #content_directory if that attribute is set. If the file is not already inside that directory, it will
+  be copied into an `img/` subdirectory of the #content_directory.
+
   __Options__
 
   * `slice_lines` (str) &ndash; A Python-style slice string that will slice the lines that are inserted into
@@ -31,6 +36,8 @@ class CatTagProcessor(MarkdownPreprocessor):
       @cat ../../readme.md :with slice_lines = "2:"
   """
 
+  content_directory: t.Optional[str] = None
+
   def process_files(self, files: MarkdownFiles) -> None:
     from novella.markdown.tagparser import parse_block_tags, replace_tags
     for file in files:
@@ -41,6 +48,8 @@ class CatTagProcessor(MarkdownPreprocessor):
       )
 
   def _replace_tag(self, project_directory: Path, file: MarkdownFile, build: BuildContext, tag: Tag) -> str | None:
+    """ Callback for #replace_tags(). """
+
     args = tag.args.strip()
     if args.startswith('/'):
       source_path = Path(project_directory / args[1:])
@@ -62,7 +71,37 @@ class CatTagProcessor(MarkdownPreprocessor):
       lines = eval(f'lines[{tag.options["slice_lines"]}]')
       text = '\n'.join(lines)
 
+    text = self._replace_image_references(project_directory, source_path, build, text)
+
     # Preprocess the content before returning it.
     text = self.action.repeat(file.path, file.output_path, text, source_path, self)
 
+    return text
+
+  def _replace_image_references(self, project_directory: Path, file_path: Path, build: BuildContext, text: str) -> str:
+    import re
+    from nr.util.fs import is_relative_to
+
+    assert self.action.path
+    rel_content_directory = Path(self.content_directory or self.action.path)
+    content_directory = project_directory / rel_content_directory
+
+    def _sub(match: re.Match) -> str:
+      path: str = match.group(2)
+      full_path = project_directory / file_path.parent / path
+      if not full_path.exists():
+        logger.warning('Image file <fg=yellow>%s</fg> referenced in <fg=yellow>%s</fg> not found', full_path, file_path)
+        return match.group(0)
+      build.watch(full_path)
+      if not is_relative_to(full_path, content_directory):
+        relative_path = Path('img', full_path.name)
+        target_path = build.directory / rel_content_directory / relative_path
+        target_path.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copyfile(full_path, target_path)
+      else:
+        relative_path = full_path.relative_to(content_directory)
+      return match.group(1) + str(relative_path) + match.group(3)
+
+    text = re.sub(r'(!\[[^\]]*?\]\()([^\)]+?)(\))', _sub, text)
+    text = re.sub(r'(<img.*?src=")([^"]+?)(".*?/?>)', _sub, text)
     return text
